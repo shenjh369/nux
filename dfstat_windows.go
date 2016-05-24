e75bbcd0dc8f978a78de158064bf4b1da230cdc7
shenjh <shenjianhua369@163.com>
2016-05-24 16:40:27 +0800

    add windows dfstat collect.

---
+++ dfstat_windows.go
@@ -0,0 +1,40 @@
+package nux
+
+import (
+	"github.com/shirou/gopsutil/disk"
+)
+
+func ListMountPoint() ([][3]string, error) {
+	result := make([][3]string, 0)
+	list, err := disk.DiskPartitions(true)
+	if err != nil {
+		return nil, err
+	}
+	for _, v := range list {
+		result = append(result, [3]string{v.Device, v.Mountpoint, v.Fstype})
+	}
+	return result, nil
+}
+
+func BuildDeviceUsage(_fsSpec, _fsFile, _fsVfstype string) (*DeviceUsage, error) {
+	var deviceUsage DeviceUsage
+	stat, err := disk.DiskUsage(_fsSpec)
+	if err != nil {
+		return nil, err
+	}
+	deviceUsage = DeviceUsage{_fsSpec,
+		_fsFile,
+		_fsVfstype,
+		stat.Total,
+		stat.Used,
+		stat.Free,
+		stat.UsedPercent,
+		100 - stat.UsedPercent,
+		stat.InodesTotal,
+		stat.InodesUsed,
+		stat.InodesFree,
+		stat.InodesUsedPercent,
+		100 - stat.InodesUsedPercent,
+	}
+	return &deviceUsage, nil
+}
